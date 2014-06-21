cfe0c664780368b1dcde16828e0ece107db60bf6
asdoc <asdocode@gmail.com>
2014-06-21 23:21:17 +0530

    Fixed sending error for more than one message

--- chat_lib.py
+++ chat_lib.py
@@ -1,19 +1,26 @@
 import socket
 
-def recv(port):
-	serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	serversocket.bind(('', port))
-	serversocket.listen(5)          #become a server socket, maximum 5 connections
-	while True:
-		connection, address = serversocket.accept()
-		buf = connection.recv(64)
-		if len(buf)> 0:
-			return buf
-			break
-	return None
+class Communication:
+
+	def __init__(self,ip,my_port,send_port):
+		self.ip = ip
+		self.my_port = my_port
+		self.send_port = send_port
+		self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.serversocket.bind(('', self.my_port))
+		
+	def recv(self):
+		self.serversocket.listen(5)
+		while True:
+			connection, address = self.serversocket.accept()
+			buf = connection.recv(64)
+			if len(buf)> 0:
+				return buf
+				break
+		return None
 	
-def send(ip,port,msg):
-	clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	clientsocket.connect((ip, port))
-	clientsocket.send(msg)
+	def send(self,msg):
+		clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		clientsocket.connect((self.ip, self.send_port))
+		clientsocket.send(msg)
 
